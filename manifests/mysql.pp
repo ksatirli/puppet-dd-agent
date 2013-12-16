230db58c6bdb747256b859ab9664ac897c0321de
Kerim Satirli <k.satirli@synapticate.com>
2013-12-16 16:19:00 +0100

    PUPPET-1 adds MySQL module

---
+++ manifests/mysql.pp
@@ -0,0 +1,27 @@
+# manage configuration for dd-agent MySQL integration
+define dd_agent::mysql(
+  $server,
+  $user           = undef,
+  $pass           = undef,
+  $port           = undef,
+  $defaults_file  = undef,
+  $replication    = undef,
+  $galera_cluster = undef
+  ) {
+
+  # check if parent is available
+  if !defined(Class['dd_agent']) {
+    fail('The dd_agent base class must be included before this module can be used')
+  }
+
+  $app = 'mysql'
+
+  file { "${dd_agent::config_dir}/${app}.yaml":
+    ensure  => 'file',
+    backup  => '.puppetold',
+    content => template("dd_agent/${app}.yaml.erb"),
+    replace => true,
+    notify  => Service['datadog-agent']
+  }
+
+}
