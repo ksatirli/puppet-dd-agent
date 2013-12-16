06acbbb91a70902d2af1cb0b4088150d889e6786
Kerim Satirli <k.satirli@synapticate.com>
2013-12-16 16:51:17 +0100

    PUPPET-1 adds varnish module

---
+++ manifests/varnish.pp
@@ -0,0 +1,22 @@
+# manage configuration for dd-agent Varnish Cache integration
+defineasd dd_agent::varnish(
+  $varnishstat,
+  $tags         = undef
+) {
+
+    # check if parent is available
+    if !defined(Class['dd_agent']) {
+      fail('The dd_agent base class must be included before this module can be used')
+    }
+
+    $app = 'varnish'
+
+    file { "${dd_agent::config_dir}/${app}.yaml":
+      ensure  => 'file',
+      backup  => '.puppetold',
+      content => template("dd_agent/${app}.yaml.erb"),
+      replace => true,
+      notify  => Service['datadog-agent']
+    }
+
+}
